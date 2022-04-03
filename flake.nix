41dca65b049db5e7bd1652f0d0f946d60546a512
Ilan Joselevich <personal@ilanjoselevich.com>
2022-04-03 21:25:55 +0300

    add apps

--- flake.nix
+++ flake.nix
@@ -7,14 +7,34 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShell.${system} = pkgs.mkShell {
-        packages = with pkgs; [ (terraform.withPlugins (plugins: [ plugins.libvirt ])) ];
-      };
-      defaultPackage.${system} = terranix.lib.terranixConfiguration {
+      terraform = (pkgs.terraform.withPlugins (plugins: [ plugins.libvirt ]));
+      terraformConfiguration = terranix.lib.terranixConfiguration {
         inherit system;
         modules = [ ./config.nix ];
+      };
+    in
+    {
+      defaultPackage.${system} = terraformConfiguration;
+      devShell.${system} = pkgs.mkShell { packages = with pkgs; [ terraform terranix.defaultPackage.${system} ]; };
+      apps.${system} = {
+        apply = {
+          type = "app";
+          program = toString (pkgs.writers.writeBash "apply" ''
+            if [[ -e config.tf.json ]]; then rm -f config.tf.json; fi
+            cp ${terraformConfiguration} config.tf.json \
+              && ${terraform}/bin/terraform init \
+              && ${terraform}/bin/terraform apply
+          '');
+        };
+        destroy = {
+          type = "app";
+          program = toString (pkgs.writers.writeBash "destroy" ''
+            if [[ -e config.tf.json ]]; then rm -f config.tf.json; fi
+            cp ${terraformConfiguration} config.tf.json \
+              && ${terraform}/bin/terraform init \
+              && ${terraform}/bin/terraform destroy
+          '');
+        };
       };
     };
 }
